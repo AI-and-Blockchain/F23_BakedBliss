fff806f54c5b0ca6620a9675fc9514b704b9c5b5
dfuen010 <dfuen010@gmail.com>
2023-11-17 01:37:58 -0500

    Updated structure & insertion of recipe similarities in Recommendation Class

--- recommendations/Recommendation.py
+++ recommendations/Recommendation.py
@@ -1,14 +1,47 @@
+import csv
+from surprise import Dataset, Reader
+from surprise.model_selection import train_test_split
+from surprise import SVD
+from surprise import accuracy
+
 class Recommendation:
     def __init__(self, rid):
         self.rid = rid
         #this will be a list of tuples with tuple[0] being other recipes as objects and tuple
         #[1] being a number that is the similarity between this recipe and the recipe in the tuple
         self.similarities = list()
+        self.recommendDataFile = open('recommend_data.csv', 'w', newline='')
+        self.datawriter = csv.writer(self.recommendDataFile)
     
     #function that will retrieve all recipes in the current DB and determine a similarity between 
     #them for user recommendations
-    def defineSimilarities(self, recipes):
-        print(recipes)
+    def defineSimilarities(self, model, recipe1, recipe2):
+        user_id = 0  # Using a single user for simplicity
+        items_to_recommend = list(range(max(len(recipe1.ingredients), len(recipe2.ingredients))))
+        
+        # Convert Recipe objects to Surprise testset format
+        user_item_pairs = [(user_id, item_id, 0) for item_id in items_to_recommend]  # Using a constant rating of 0
+        
+        # Predict ratings for the user-item pairs
+        user_item_predictions = model.test(user_item_pairs)
+        
+        # Extract predicted ratings
+        predicted_ratings = [prediction.est for prediction in user_item_predictions]
+        
+        # Calculate similarity based on predicted ratings
+        similarity = sum(predicted_ratings) / len(predicted_ratings)
+        
+        return similarity
+
+    def writeNewSimilarityForRecipe(self, r2data):
+        #this function will be to write in the csv file the similarity rating calculated from
+        #defineSimilarites function between rid (this current recipe) and rid2 (the recipe being
+        #compared)
+        #Future: add these similarity ratings to IPFS Node information about the current recipe
+        None
+
+    def closeCSVFile(self):
+        self.recommendDataFile.close()
 
     def __str__(self):
         return str(self.rid) + '\n' + str(self.similarities)
@@ -16,3 +49,4 @@ class Recommendation:
 
 r1 = Recommendation(23)
 print(r1)
+r1.closeCSVFile()
--- recommendations/recommender.py
+++ recommendations/recommender.py
@@ -3,25 +3,29 @@ from surprise import Dataset, Reader
 from surprise import SVD
 from surprise.model_selection import train_test_split
 from surprise import accuracy
+import csv
+from pathlib import Path
 
-# Load your data into a Pandas DataFrame (replace with your data)
-data = pd.read_csv("recommend_data.csv")  # Columns: user_id, item_id, rating
+# Load your data into a Pandas DataFrame
+script_dir = Path(__file__).resolve().parent
+file_path = script_dir / 'recommend_data.csv'
+df = pd.read_csv(file_path)  # Columns: rid, rid2, rating
 
 # Create a Surprise Dataset from the DataFrame
 reader = Reader(rating_scale=(1, 5))  # Define the rating scale
-data = Dataset.load_from_df(data[['user_id', 'item_id', 'rating']], reader)
+data = Dataset.load_from_df(df[['rid', 'rid2', 'rating']], reader)
 
-# Split the data into a training and testing set
-trainset, testset = train_test_split(data, test_size=0.2)
+# convert data into a full trainset
+trainset = data.build_full_trainset()
 
 # Create and train the SVD model
 model = SVD()
 model.fit(trainset)
 
 # Recommend items for a specific user
-user_id = 1
-items_to_recommend = set(data.df['item_id']) - set(data.df[data.df['user_id'] == user_id]['item_id'])
-user_item_pairs = [(user_id, item, 0) for item in items_to_recommend]  # Use a constant rating of 0
+rid = 1
+items_to_recommend = set(data.df['rid2']) - set(data.df[data.df['rid'] == rid]['rid2'])
+user_item_pairs = [(rid, item, 0) for item in items_to_recommend]  # Use a constant rating of 0
 
 # Predict ratings for the user-item pairs
 user_item_predictions = model.test(user_item_pairs)
@@ -30,4 +34,4 @@ user_item_predictions = model.test(user_item_pairs)
 user_item_predictions.sort(key=lambda x: x.est, reverse=True)
 top_n = 10  # Number of recommendations
 recommended_items = [prediction.iid for prediction in user_item_predictions[:top_n]]
-print("Top {} recommendations for user {}: {}".format(top_n, user_id, recommended_items))
+print("Top {} recommendations for user {}: {}".format(top_n, rid, recommended_items))
