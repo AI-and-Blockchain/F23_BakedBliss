f1c05f3d6b21e27c5506637e0aaf465b0e1639d4
Andrew Franco <andrew.franco@clavetech.com>
2023-11-27 23:13:32 -0500

    Update views.py
    updated new views

--- src/website/views.py
+++ src/website/views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    pass
+    return render_template("home.html")
+
+@views.route('/upload')
+def upload():
+    return render_template("upload.html")
