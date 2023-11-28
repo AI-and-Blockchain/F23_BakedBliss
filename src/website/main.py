a94dd8af80ce86328dc2ceef8b835329df454bf1
Andrew Franco <andrew.franco@clavetech.com>
2023-11-27 23:13:05 -0500

    Update main.py
    added registration for blueprints

--- src/website/main.py
+++ src/website/main.py
@@ -4,4 +4,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'kjfs^&*^*&jska'
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
